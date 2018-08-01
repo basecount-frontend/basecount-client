ad0d2fb43b3137e4061cc90b09efef01668f2343
Filip Stepien <contact@filipstepien.com>
2018-08-01 15:45:16 -0400

    start working on /app/admin/users

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,7 +1,30 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Section, ButtonPrimary, ButtonSecondary, Icon } from "../utilities";
 
 export default class Users extends Component {
   render() {
-    return <div>Edit users accounts here</div>;
+    return (
+      <UserSection>
+        <div className="section-wrapper">
+          <div className="users__filter">
+            <input type="search" />
+            <Icon name="filter" />
+          </div>
+
+          <div className="users__item">
+            <Link to="/app/admin/users/user/:id">
+              <div className="users__item__orgLink">
+                Dummy Org <span>></span>{" "}
+              </div>
+              <div className="users__item">Dummy Manager</div>
+            </Link>
+            <ButtonPrimary>Add User</ButtonPrimary>
+          </div>
+        </div>
+      </UserSection>
+    );
   }
 }
+
+const UserSection = Section.extend``;
