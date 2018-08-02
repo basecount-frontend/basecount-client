d8717bda3eb4627e0858e43a773d375d1bbae28d
Scott Agirs <scott.agirs@gmail.com>
2018-08-01 20:09:25 -0400

    Rename UserItem.js to Users.js; Contains all components for Users Page

---
+++ src/utilities/Users.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const UserItemContainer = styled.div`
+  width: 90%;
+  max-width: 400px;
+  margin: 0 auto;
+`;
+
+export const UserItem = styled.div`
+  width: 90%;
+  background-color: #1c41b2;
+  height: 64px;
+  margin: 4px auto;
+  border: 1px solid transparent;
+  border-radius: 2px;
+  text-aling: left;
+`;
+
+export const UserItemTitle = styled.div`
+  text-transform: uppercase;
+  font-weight: 800;
+  color: white;
+`;
+
+export const UserItemRole = styled.div`
+  color: white;
+  font-weight: 300;
+  font-size: 1.4rem;
+`;
